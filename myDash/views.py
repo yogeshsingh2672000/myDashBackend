e30a93d759ccf52640fc1a512a5a32ed84eb647d
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-05-23 00:03:16 +0530

    made a html page and render it

--- myDash/views.py
+++ myDash/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello World");
+    # return HttpResponse("Hello World");
+    return render(request, "hello.html", {"name": "Yogesh"})
